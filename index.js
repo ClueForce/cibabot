85561c6fb109323d34ac6a3fc5484d0324c935d2
ClueForce <73283806+ClueForce@users.noreply.github.com>
2021-04-05 17:30:07 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,80 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+const config = require('./config.json');
+client.config = config
+const newUsers = [];
+
+//testing welcome
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
+
+//ClueForce Uptime//
+
+const http = require("http");
+const express = require("express");
+const app = express();
+var server = http.createServer(app);
+
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+  
+// Init discord giveaways 
+const { GiveawaysManager } = require('discord-giveaways');
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./database.json",
+    updateCountdownEvery: 3000,
+    default: {
+        botsCanWin: false,
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+//End 
+
+/* Load all events */
+fs.readdir("./events/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
+        console.log(`👌 Event loaded: ${eventName}`);
+        client.on(eventName, event.bind(null, client));
+        delete require.cache[require.resolve(`./events/${file}`)];
+    });
+});
+
+client.commands = new Discord.Collection();
+
+/* Load all commands */
+fs.readdir("./commands/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        client.commands.set(commandName, props);
+        console.log(`👌 Command loaded: ${commandName}`);
+    });
+});
+
+// Login through the client 
+  client.login(process.env.TOKEN);
