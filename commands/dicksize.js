ad8aeffeae049101420614de62e81833763fdb90
ClueForce <73283806+ClueForce@users.noreply.github.com>
2021-04-05 17:34:22 +0700

    Create dicksize.js

---
+++ commands/dicksize.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+module.exports.run = async (client, message, args) => {
+    if (message.author.bot) return;
+    let prefix = config.prefix;
+    if(!message.content.startsWith(prefix)) return;   
+
+let sizes = [
+      "8D",
+      "8=D",
+      "8==D",
+      "8===D",
+      "8====D",
+      "8=====D",
+      "8======D",
+      "8=======D",
+      "8========D",
+      "8=========D",
+      "8==========D",
+      "8===========D",
+      "8============D",
+      "8=============D",
+      "8==============D",
+      "8===============D",
+      "8================D",
+      "8==================D"
+    ];
+
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.member;
+
+    let Result = sizes[Math.floor(Math.random() * sizes.length)];
+
+    let embed = new Discord.MessageEmbed()
+      .setColor('RED')
+      .setTitle(`Pp v2 Machine`)
+      .setDescription(`${Member.user.username} pp Size Is\n${Result}`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+
+    //End
+  }
